316adbec6bfb2d2a7e739c893b17b2485637c0e6
BourbonDefi <80276717+bourbondefi@users.noreply.github.com>
2021-05-02 13:12:22 -0400

    bourbon

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -3002,22 +3002,22 @@ var templateObject_1$M;
 
 var baseColors = {
     failure: "#cd3e5f",
-    primary: "#41aa29",
-    primaryBright: "#50aa29",
-    primaryDark: "#348c22",
-    secondary: "#45d9bb",
-    success: "#31D0AA",
+    primary: "#bc6e16",
+    primaryBright: "#bc6e16",
+    primaryDark: "#bc6e16",
+    secondary: "#c49700",
+    success: "#bc6e16",
     warning: "#FFB237",
 };
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#ffffff", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", inputSecondary: "#d7caec", tertiary: "#EFF4F5", text: "#424f3e", textDisabled: "#BDC2C4", textSubtle: "#62815c", borderColor: "#E9EAEB", card: "#FFFFFF", nav: "#f9fdf9", gradients: {
-        bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
-    } });
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#ffffff", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", inputSecondary: "#d7caec", tertiary: "#EFF4F5", text: "#bc6e16", textDisabled: "#BDC2C4", textSubtle: "#bc6e16", borderColor: "#E9EAEB", card: "#FFFFFF", nav: "#f9fdf9", gradients: {
+    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+} });
 var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#9A6AFF", background: "#100C18", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", inputSecondary: "#66578D", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#A28BD4", borderColor: "#524B63", card: "#27262c", nav: "#27262c", gradients: {
-        bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
-    } });
+    bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+} });
 
 var light = {
     background: lightColors.card,
